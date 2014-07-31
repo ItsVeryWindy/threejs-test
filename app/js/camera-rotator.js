80fc82889bf53d17e393e271fe44f8ee3bb2b4dc
ItsVeryWindy <ItsVeryWindy@users.noreply.github.com>
2014-07-31 22:03:59 +0100

    fixed up blur effects

--- app/js/camera-rotator.js
+++ app/js/camera-rotator.js
@@ -1,8 +1,8 @@
 /*global define:false */
 
 define(['three', 'pubsub', 'tween', 'postRenderer'], function (THREE, PubSub, TWEEN, PostRenderer) {
-	var yaw = 0, pitch = 0, currentTween = null, postRenderer,
-		keyPressed = {};
+	var yaw = 0, pitch = 0, currentTween = null, postRendererH, postRendererV,
+		keyPressed = {}, hBlur = 0, vBlur = 0, spotLight;
 	
 	/**
 	* @author zz85 / http://www.lab4games.net/zz85/blog
@@ -55,6 +55,53 @@ define(['three', 'pubsub', 'tween', 'postRenderer'], function (THREE, PubSub, TW
 		].join('\n')
 	};
 	
+	/**
+	 * @author zz85 / http://www.lab4games.net/zz85/blog
+	 *
+	 * Two pass Gaussian blur filter (horizontal and vertical blur shaders)
+	 * - described in http://www.gamerendering.com/2008/10/11/gaussian-blur-filter-shader/
+	 *   and used in http://www.cake23.de/traveling-wavefronts-lit-up.html
+	 *
+	 * - 9 samples per pass
+	 * - standard deviation 2.7
+	 * - 'h' and 'v' parameters should be set to '1 / width' and '1 / height'
+	 */
+
+	THREE.VerticalBlurShader = {
+		uniforms: {
+			'tDiffuse': { type: 't', value: null },
+			'v':        { type: 'f', value: 1.0 / 512.0 }
+		},
+		vertexShader: [
+			'varying vec2 vUv;',
+			'void main() {',
+				'vUv = uv;',
+				'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+			'}'
+		].join('\n'),
+		fragmentShader: [
+			'uniform sampler2D tDiffuse;',
+			'uniform float v;',
+			'varying vec2 vUv;',
+			'void main() {',
+				'vec4 sum = vec4( 0.0 );',
+
+				'sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y - 4.0 * v ) ) * 0.051;',
+				'sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y - 3.0 * v ) ) * 0.0918;',
+				'sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y - 2.0 * v ) ) * 0.12245;',
+				'sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y - 1.0 * v ) ) * 0.1531;',
+				'sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y ) ) * 0.1633;',
+				'sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y + 1.0 * v ) ) * 0.1531;',
+				'sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y + 2.0 * v ) ) * 0.12245;',
+				'sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y + 3.0 * v ) ) * 0.0918;',
+				'sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y + 4.0 * v ) ) * 0.051;',
+
+				'gl_FragColor = sum;',
+
+			'}'
+		].join('\n')
+	};
+	
 	function onKeyDown(event) {
 		var LEFT = 37, UP = 38, RIGHT = 39, BOTTOM = 40;
 
@@ -65,16 +112,43 @@ define(['three', 'pubsub', 'tween', 'postRenderer'], function (THREE, PubSub, TW
 		var yawDir = 0;
 		var pitchDir = 0;
 		
+		if(vBlur === 0 && (event.keyCode === UP || event.keyCode === BOTTOM)) {
+			pitch = Math.round(pitch / 90) * 90;
+		}
+		
+		if(hBlur === 0 && (event.keyCode === LEFT || event.keyCode === RIGHT)) {
+			yaw = Math.round(yaw / 90) * 90;
+		}
+		
+		function tween(valueDir, getValue, setValue, setBlur) {
+			var newValue = getValue() + valueDir;
+			var pValue = 0;
+			var oldValue = getValue();
+			
+			currentTween = new TWEEN.Tween( { x: 0 } )
+				.to( { x: valueDir }, 1000 )
+				.easing(TWEEN.Easing.Exponential.Out)
+				.onUpdate(function () {
+					setValue(getValue() + this.x - pValue);
+					pValue = this.x;
+					setBlur(Math.abs(newValue - oldValue - this.x) / 90 * 16);
+				})
+				.onComplete(function () {
+					currentTween = null;
+				})
+				.start();
+		}
+		
 		switch (event.keyCode) {
 			case UP:
-				if(pitch >= 0) {
+				//if(pitch >= 0) {
 					pitchDir = -90;
-				}
+				//}
 			break;
 			case BOTTOM:
-				if(pitch <= 0) {
+				//if(pitch <= 0) {
 					pitchDir = 90;
-				}
+				//}
 			break;
 			case LEFT:
 				yawDir = 90;
@@ -84,18 +158,16 @@ define(['three', 'pubsub', 'tween', 'postRenderer'], function (THREE, PubSub, TW
 			break;
 		}
 		
-		if(!currentTween && (pitchDir !== 0 || yawDir !== 0)) {
-			currentTween = new TWEEN.Tween( { x: yaw, y: pitch } )
-				.to( { x: yaw + yawDir, y: pitch + pitchDir }, 1000 )
-				.easing(TWEEN.Easing.Exponential.Out)
-				.onUpdate(function () {
-					yaw = this.x;
-					pitch = this.y;
-				})
-				.onComplete(function () {
-					currentTween = null;
-				})
-				.start();
+		if(yawDir !== 0) {
+			tween(yawDir, function() { return yaw; }, function(value) { yaw = value; }, function(value) { hBlur = value; });
+		}
+		
+		if(pitchDir !== 0) {
+			tween(pitchDir, function() { return pitch; }, function(value) {
+				pitch = value;
+				pitch = Math.min(pitch, 90);
+				pitch = Math.max(pitch, -90);
+			}, function(value) { vBlur = value; });
 		}
 		
 		keyPressed[event.keyCode.toString()] = true;
@@ -105,17 +177,39 @@ define(['three', 'pubsub', 'tween', 'postRenderer'], function (THREE, PubSub, TW
 		delete keyPressed[event.keyCode.toString()];
 	}
   
-  PubSub.subscribe('init-scene', function() {
+  PubSub.subscribe('init-scene', function(msg, data) {
 	
-	postRenderer = new PostRenderer(
+	postRendererH = new PostRenderer(
 		THREE.HorizontalBlurShader,
 		function(uniforms, target) {
 			uniforms.tDiffuse.value = target();
-			//uniforms.h.value = 1.0 / window.innerWidth;
+			uniforms.h.value = hBlur / window.innerWidth;
 		},
 		function(uniforms, render) {
 			render(null, uniforms.tDiffuse.value);
+			uniforms.h.value = hBlur / window.innerWidth;
 		});
+		
+	postRendererV = new PostRenderer(
+		THREE.VerticalBlurShader,
+		function(uniforms, target) {
+			uniforms.tDiffuse.value = target();
+			uniforms.v.value = vBlur / window.innerWidth;
+		},
+		function(uniforms, render) {
+			render(null, uniforms.tDiffuse.value);
+			uniforms.v.value = vBlur / window.innerWidth;
+		});
+	
+	spotLight = new THREE.SpotLight( 0xffffff, 0.5 );
+	spotLight.position.set(data.camera.x, data.camera.y, data.camera.z);
+	spotLight.castShadow = true;
+	spotLight.shadowMapWidth = 1024;
+	spotLight.shadowMapHeight = 1024;
+	spotLight.shadowCameraNear = 500;
+	spotLight.shadowCameraFar = 4000;
+	spotLight.shadowCameraFov = 30;
+	data.scene.add(spotLight);
 	
 	window.addEventListener('keydown', onKeyDown, false);
 	window.addEventListener('keyup', onKeyUp, false);
@@ -134,10 +228,26 @@ define(['three', 'pubsub', 'tween', 'postRenderer'], function (THREE, PubSub, TW
 
 	camera.translateZ(1000);
 	
+	spotLight.position.x = camera.position.x;
+	spotLight.position.y = camera.position.y;
+	spotLight.position.z = camera.position.z;
+	
 	data.camera = camera;
 	
-	if(currentTween) {
-		postRenderer.render(data);
+	if(vBlur === 0) {
+		pitch = Math.round(pitch / 90) * 90;
+	}
+	
+	if(hBlur === 0) {
+		yaw = Math.round(yaw / 90) * 90;
+	}
+	
+	if(hBlur) {
+		postRendererH.render(data);
+	}
+	
+	if(vBlur) {
+		postRendererV.render(data);
 	}
   });
 });
